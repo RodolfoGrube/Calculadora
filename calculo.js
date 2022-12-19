a9bab26c606ee79df99c403e1b5a9bc0d25838e1
Rodolfo Grube <rodolfogrubeneto@gmail.com>
2022-12-19 13:02:53 -0300

    certo

--- calculo.js
+++ calculo.js
@@ -1,9 +1,9 @@
 function multi(){
     var n1 = document.getElementById ("numer1").value;
-    var n2 = document;getElementById ("numer2").value;
+    var n2 = document.getElementById ("numer2").value;
 
     var resultado = n1 * n2;
 
-    document.getElementById ("resultado") . innerHTML("Resultado: ". resultado)
+    document.getElementById ("resultado") . innerHTML = "Resultado: " + resultado
 
 }
