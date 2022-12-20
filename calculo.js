c90730e549a2630c3ea31bab18f0cb5aa1c5164b
Rodolfo Grube <rodolfogrubeneto@gmail.com>
2022-12-20 10:12:20 -0300

    todos os calculos

--- calculo.js
+++ calculo.js
@@ -1,6 +1,6 @@
 function multi(){
-    var n1 = document.getElementById ("numer1").value;
-    var n2 = document.getElementById ("numer2").value;
+    var n1 = parseInt(document.getElementById ("numer1").value);
+    var n2 = parseInt(document.getElementById ("numer2").value);
 
     var resultado = n1 * n2;
 
@@ -8,11 +8,29 @@ function multi(){
 
 }
 function divi(){
-    var n1 = document.getElementById ("numer1").value;
-    var n2 = document.getElementById ("nuner2").value;
+    var n1 = parseInt(document.getElementById ("numer1").value);
+    var n2 = parseInt(document.getElementById ("numer2").value);
     
     var resultado = n1 / n2;
     
-    document.getElementById ("Resultado"). innerHTML = "Resultado: " + resultado;
+    document.getElementById ("resultado"). innerHTML = "Resultado: " + resultado;
+
+}
+function sub(){
+    var n1 = parseInt(document.getElementById ("numer1").value);
+    var n2 = parseInt(document.getElementById ("numer2").value);
+
+    var resultado = n1 - n2;
+
+    document.getElementById ("resultado"). innerHTML = "Resultado: " + resultado;
+
+}
+function soma(){
+    var n1 = parseInt(document.getElementById ("numer1").value);
+    var n2 = parseInt(document.getElementById ("numer2").value);
+
+    var resultado = n1 + n2;
+
+    document.getElementById ("resultado") . innerHTML = "Resultado: " + resultado;
 
 }
